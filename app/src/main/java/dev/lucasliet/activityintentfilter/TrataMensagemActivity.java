903f714b4f0abfed3f09f52fedbea79914117a39
lucasliet <lucasouliveira@gmail.com>
2020-09-28 22:05:58 -0300

    initial

---
+++ app/src/main/java/dev/lucasliet/activityintentfilter/TrataMensagemActivity.java
@@ -0,0 +1,14 @@
+package dev.lucasliet.activityintentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrataMensagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trata_mensagem);
+    }
+}

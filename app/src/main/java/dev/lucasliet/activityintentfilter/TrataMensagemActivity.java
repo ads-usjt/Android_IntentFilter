a302c56a552a768b737a3bc122adbeeeca450623
lucasliet <lucasouliveira@gmail.com>
2020-09-28 22:09:52 -0300

    receive message and toast it

--- app/src/main/java/dev/lucasliet/activityintentfilter/TrataMensagemActivity.java
+++ app/src/main/java/dev/lucasliet/activityintentfilter/TrataMensagemActivity.java
@@ -2,7 +2,9 @@ package dev.lucasliet.activityintentfilter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TrataMensagemActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class TrataMensagemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trata_mensagem);
+        Intent origemIntent = getIntent();
+        String mensagem = origemIntent.getStringExtra(Intent.EXTRA_TEXT);
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
     }
 }
